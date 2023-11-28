263bcac1ff8fa0b0419693c1cc0870b24da1a9b8
AbdulAhad696 <abdulahad9992@gmail.com>
2023-11-28 22:41:53 +0500

    Frontend Completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,158 @@
+'use client'
 import Image from 'next/image'
+import React, { useState, useEffect, useRef } from 'react';
+import TextField from '@mui/material/TextField';
+import { Grid, IconButton } from '@mui/material';
+import { Send } from '@mui/icons-material';
+// import { DiscussServiceClient } from "@google-ai/generativelanguage";
 
-export default function Home() {
+// import { GoogleAuth } from "google-auth-library";
+const Home = () => {
+  const [query, setQuery] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState('');
+  const originalText= "Hello, I am Bard, your programming assistant. I can help you with your programming queries.";
+  const chat=[];
+  const bottomRef = useRef<HTMLDivElement>(null);
+  
+  const API_KEY = "AIzaSyDPeP5nqXqDtS3bOCxWT6dYn8t-Sgpmeos";
+  
+  useEffect(() => {
+    let index = 0;
+    const intervalId = setInterval(() => {
+      if (index <= originalText.length) {
+        setText(originalText.substring(0, index));
+        index++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 100); // Adjust the speed of typing by changing the interval duration (in milliseconds)
+    
+    return () => {
+      clearInterval(intervalId); // Clear the interval on component unmount
+    };
+  }, [originalText]);
+
+  useEffect(() => {
+    if (bottomRef.current) {
+      bottomRef.current.scrollIntoView({ behavior: 'smooth' });
+      // Change the behavior to 'auto' if you prefer an immediate scroll without smooth animation.
+      // bottomRef.current.scrollIntoView({ behavior: 'auto' });
+    }
+  }, [messages]);
+
+  const MODEL_NAME = "models/chat-bison-001";
+
+  // const client = new DiscussServiceClient({
+  //   authClient: new GoogleAuth().fromAPIKey(API_KEY),
+  // });
+
+  const processQuery=async (query:string)=> {
+    // chat.push({content:query});
+    // const result = await client.generateMessage({
+    //   model: MODEL_NAME, // Required. The model to use to generate the result.
+    //   temperature: 0.1, // Optional. Value `0.0` always uses the highest-probability result.
+    //   candidateCount: 1, // Optional. The number of candidate results to generate.
+    //   prompt: {
+    //     // optional, preamble context to prime responses
+    //     context: "You are a programming assistant only.",
+    //     // Optional. Examples for further fine-tuning of responses.
+    //     examples: [
+    //       {
+    //         input: { content: "Write a program for factorial using recursion in python" },
+    //         output: {
+    //           content:
+    //             `Sure, here are examples of factorial calculation using both recursion and iteration (without recursion) in Python:
+    //             def factorial_recursive(n):
+    //               if n == 0 or n == 1:
+    //                   return 1
+    //               else:
+    //                   return n * factorial_recursive(n - 1)
+
+    //             # Example usage:
+    //             number = 5
+    //             result_recursive = factorial_recursive(number)
+    //             print(f"The factorial of {number} using recursion is: {result_recursive}")
+    //             The recursive approach involves calling the function within itself until it reaches the base case (n = 0 or n = 1)`,
+    //         },
+    //       },
+    //     ],
+    //     // Required. Alternating prompt/response messages.
+    //     messages: [{ content: query }],
+    //   },
+    // });
+
+    // console.log("worked");
+    // setMessages((messages) => [...messages, { text: result?.[0]?.candidates?.[0]?.content ?? '', user: 'Bard' }]);
+  }
+  
+  const handleQueryChange = (event: { target: { value: string; }; }) => {
+    setQuery(() => event.target.value);
+  }
+  const handleSubmit = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && query !== "") {
+      setMessages((messages) => [...messages, { text: query, user: 'You' }]);
+      setQuery(() => "");
+      processQuery(query);
+    }
+  }
+  const handleClick = () => {
+    if (query !== "") {
+      setMessages((messages) => [...messages, { text: query, user: 'You' }]);
+      setQuery(() => "");
+      processQuery(query);
+    }
+
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <React.Fragment>
+      
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="typing-animation font-bold">
+      <span style={{fontSize:'24px'}}>{text}</span>
       </div>
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Get started by typing in&nbsp;
+            <code className="font-mono font-bold">your Query</code>
+          </p>
+          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <a
+              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 text-lg"
+              href="https://ai.google/discover/palm2/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered By{' '}
+              <Image
+                src="/logo.svg"
+                alt="PaLM Logo"
+                className="dark:invert"
+                width={75}
+                height={16}
+                priority
+              />
+            </a>
+          </div>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
+
+        {messages.length>0 ?
+          <Grid container sx={{ background: 'white', width: '70vw', borderRadius: '10px' }}>
+            {messages?.map((message, index) => (
+              <Grid container key={index} sx={{ padding: '20px' }}>
+                <Grid item>
+                  <span className='font-bold'>{message?.user}:&nbsp;</span>
+                </Grid>
+                <Grid item>
+                  <p>{message?.text}</p>
+                </Grid>
+              </Grid>
+            ))}
+          </Grid>
+          : <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+             <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
@@ -37,77 +160,23 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
+          </div>}
+        <div ref={bottomRef} />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <Grid sx={{ zIndex: 1000, marginY: '10px ' }} container alignItems={'center'} >
+          <Grid item xs={1}></Grid>
+          <Grid item xs={10} >
+            <TextField onKeyDown={handleSubmit} fullWidth id="outlined-basic" label="Query" variant="outlined" value={query} onChange={handleQueryChange} />
+          </Grid>
+          <Grid item xs={1}>
+            <IconButton aria-label="search" onClick={handleClick}>
+              <Send sx={{ color: 'blueviolet' }} />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </main>
+    </React.Fragment>
   )
 }
+
+export default Home;
